8e91a49e255875d24e00950180a1a33a3e39449f
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2016-09-24 21:33:02 -0300

    Yeah dubins!

--- gl2d/external/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/willian/GitHub/lib/gl2d/external/googletest/googletest
-# Build directory: /home/willian/GitHub/lib/gl2d/external/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
